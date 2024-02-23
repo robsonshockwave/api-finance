360cbfb6b27cc238f8d4accdd99b39b0484e80b8
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-22 22:23:11 -0300

    config knex

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify';
+import knex from 'knex';
 
 const app = fastify();
 
-app.get('/hello', () => {
-  return { hello: 'world' };
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*');
+
+  return tables;
 });
 
 app.listen({ port: 3333 }).then(() => {
