2b949746c7727fb570388a672daae2095e831300
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-22 23:38:42 -0300

    rota de criação de transações

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
 import fastify from 'fastify';
 import { knex } from './database';
 import { env } from './env';
+import { transactionsRoutes } from './routes/transactions';
 
 const app = fastify();
 
-app.get('/hello', async () => {
-  //   const tables = await knex('sqlite_schema').select('*');
-
-  const transaction = await knex('transactions')
-    .where('amount', 500)
-    .select('*');
-
-  return transaction;
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 });
 
 app.listen({ port: env.PORT }).then(() => {
