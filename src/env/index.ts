1b3118f00f7fccb7b72eeec1fd9d4b1537515c13
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-23 22:15:46 -0300

    teste e2e para criação de uma nova transação

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,11 +2,9 @@ import 'dotenv/config';
 import { z } from 'zod';
 
 const envSchema = z.object({
-  NODE_ENV: z
-    .enum(['development', 'teste', 'production'])
-    .default('production'),
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 });
 
 const _env = envSchema.safeParse(process.env);
