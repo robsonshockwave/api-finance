0cdf133265dd43c9aa34b52510b3605afccaca7c
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-23 21:19:17 -0300

    configurando um hook global para a rota transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,6 +5,10 @@ import crypto from 'node:crypto';
 import { checkSessionIdExists } from '../middlewares/check-session-id-exists';
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.addHook('preHandler', async (request, reply) => {
+    console.log(`[${request.method}] ${request.url}`);
+  });
+
   app.get(
     '/',
     {
